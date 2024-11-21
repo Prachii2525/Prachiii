23938b1f9dae6ab901117261da9896765ea2c229
Prachiii18 <prachijanjire@gmail.com>
2024-11-21 13:58:34 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
 
 
 const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
 
-           <h1 className="text-3xl text-white underline">Hello Three.js</h1>
+           <Navbar />
         </main>
         
 
